fed3248f8c162b4730224d8bf89c1b60c72d91d3
Sebastian Daniel Lovisolo <sebalovi12@gmail.com>
2026-05-08 14:10:51 +0200

    Add BaseArticleExerciseCard component and update index.vue for article selection

---
+++ src/components/BaseArticleExerciseCard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { GermanArticle } from '@/interfaces/GermanArticle'
+
+import { ref } from 'vue'
+
+import BaseArticleCard from '@/components/BaseArticleCard.vue'
+
+const germanArticles = ['der', 'die', 'das'] as GermanArticle[]
+
+const selectedArticle = ref<GermanArticle | null>(null)
+</script>
+<template>
+  <div>
+    <div
+      class="flex gap-4 items-center justify-center"
+    >
+      <BaseArticleCard
+        v-for="article in germanArticles"
+        :key="article"
+        :article="article"
+        @click="selectedArticle = article"
+      />
+    </div>
+  </div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,24 +1,11 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
-import type { GermanArticle } from '@/interfaces/GermanArticle'
-
-import BaseArticleCard from '@/components/BaseArticleCard.vue'
-
-const germanArticles = ['der', 'die', 'das'] as GermanArticle[]
-
-const selectedArticle = ref<GermanArticle | null>(null)
+import BaseArticleExerciseCard from '@/components/BaseArticleExerciseCard.vue'
 </script>
 
 <template>
   <div
     class="flex gap-4 items-center justify-center"
   >
-    <BaseArticleCard
-      v-for="article in germanArticles"
-      :key="article"
-      :article="article"
-      @click="selectedArticle = article"
-    />
+    <BaseArticleExerciseCard />
   </div>
 </template>
