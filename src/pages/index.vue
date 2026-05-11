4eed8707eed51e1902fd69ac5ea03a8d23433b7c
Sebastian Daniel Lovisolo <sebalovi12@gmail.com>
2026-05-11 13:03:40 +0200

    Implement navigation buttons on main and practice pages for improved user experience
    - Added buttons to navigate to the nouns and practice sections on the main page.
    - Introduced a button to access articles from the practice page, enhancing navigation flow.
    - Utilized icons from Lucide for better visual representation of actions.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,38 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+import { Dumbbell, List } from '@lucide/vue'
+
+const router = useRouter()
 </script>
 <template>
-  <div>
-    <h1>Main Page</h1>
+  <div
+    class="flex gap-4 items-center justify-center h-full"
+  >
+    <button
+      class="btn h-32 w-32 flex items-center justify-center gap-2"
+      @click="router.push('/nouns')"
+    >
+      <List
+        class="size-4"
+        aria-hidden="true"
+      />
+      <span>
+        Nouns
+      </span>
+    </button>
+
+    <button
+      class="btn h-32 w-32 flex items-center justify-center gap-2"
+      @click="router.push('/practice')"
+    >
+      <Dumbbell
+        class="size-4"
+        aria-hidden="true"
+      />
+      <span>
+        Practice
+      </span>
+    </button>
   </div>
 </template>
--- src/pages/practice/index.vue
+++ src/pages/practice/index.vue
@@ -1,7 +1,25 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+import { Dumbbell } from '@lucide/vue'
+
+const router = useRouter()
 </script>
 <template>
-  <div>
-    <h1>Practice main page</h1>
+  <div
+    class="flex gap-4 items-center justify-center h-full"
+  >
+    <button
+      class="btn h-32 w-32 flex items-center justify-center gap-2"
+      @click="router.push('/practice/articles')"
+    >
+      <Dumbbell
+        class="size-4"
+        aria-hidden="true"
+      />
+      <span>
+        Articles
+      </span>
+    </button>
   </div>
 </template>
